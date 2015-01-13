0e2be2f81cdd530631c1ec6deab51528c3cb382d
Viktor Benei <viktor.benei@gmail.com>
2015-01-13 18:55:53 +0100

    datastore generator updated to match StepLib's

--- generators/datastore_json.rb
+++ generators/datastore_json.rb
@@ -77,89 +77,15 @@ puts "-----------"
 
 # --- UTILS ---
 
-#
-# All the input whitelist keys have to be defined
-# and the returned hash will only contain the
-#  whitelisted key-value pairs.
-def whitelist_require_hash(inhash, whitelist)
-  raise "Input hash is nil" if inhash.nil?
-
-  res_hash = {}
-  whitelist.each do |whiteitm|
-    if inhash[whiteitm].nil?
-      raise "Missing whitelisted item: #{whiteitm} | in hash: #{inhash}"
-    end
-    res_hash[whiteitm] = inhash[whiteitm]
-  end
-  return res_hash
-end
-
-#
-# Sets the provided default value for the specified
-#  key if the key is missing.
-# Defaults_arr is an array of {:key=>,:value=>} hashes
-def set_missing_defaults(inhash, defaults_arr)
-  defaults_arr.each do |a_def|
-    a_def_key = a_def[:key]
-    a_def_value = a_def[:value]
-    if inhash[a_def_key].nil?
-      inhash[a_def_key] = a_def_value
-    end
-  end
-  return inhash
-end
-
 def json_step_item_from_yaml_hash(yaml_hash)
   # set default values for optional properties
-  whitelisted = set_missing_defaults(yaml_hash, [
-    {key: 'fork_url', value: yaml_hash['website']},
-    {key: 'project_type_tags', value: []}
-    ])
-  # 
-  whitelisted = whitelist_require_hash(whitelisted, [
-    'name', 'description',
-    'website', 'fork_url',
-    'host_os_tags', 'project_type_tags', 'type_tags',
-    'is_requires_admin_user'
-    ])
-  #
-  whitelisted['source'] = whitelist_require_hash(yaml_hash['source'], ['git'])
-  if yaml_hash['inputs']
-    whitelisted['inputs'] = yaml_hash['inputs'].map {|itm|
-      whitelisted_itm = set_missing_defaults(itm, [
-        {key: 'is_expand', value: true},
-        {key: 'description', value: ''},
-        {key: 'is_required', value: false},
-        {key: 'value_options', value: []},
-        {key: 'value', value: ''},
-        {key: 'is_dont_change_value', value: false}
-        ])
-      whitelisted_itm = whitelist_require_hash(whitelisted_itm,
-        ['title', 'description', 'mapped_to', 'is_expand',
-          'is_required', 'value_options', 'value', 'is_dont_change_value'])
-      # force / convert type
-      whitelisted_itm['value_options'] = whitelisted_itm['value_options'].map { |e| e.to_s }
-      whitelisted_itm['value'] = whitelisted_itm['value'].to_s
-      # return:
-      whitelisted_itm
-    }
-  else
-    whitelisted['inputs'] = []
-  end
+  yaml_hash = Steplib::SteplibUpdater.set_defaults_for_missing_properties_in_step_version(yaml_hash)
+  # whitelist
+  yaml_hash = Steplib::SteplibValidator.whitelist_step_version(yaml_hash)
+  # validate
+  Steplib::SteplibValidator.validate_step_version!(yaml_hash)
 
-  if yaml_hash['outputs']
-    whitelisted['outputs'] = yaml_hash['outputs'].map { |itm|
-      whitelisted_itm = set_missing_defaults(itm, [
-        {key: 'description', value: ''}
-        ])
-      # return:
-      whitelist_require_hash(itm, ['title', 'description', 'mapped_to'])
-    }
-  else
-    whitelisted['outputs'] = []
-  end
-
-  return whitelisted
+  return yaml_hash
 end
 
 def default_step_data_for_stepid(stepid)
@@ -191,12 +117,10 @@ Find.find(options[:step_collection_folder]) do |path|
       stepid, stepver = match.captures
       raise 'Cant determine StepID' if stepid.nil?
       raise 'Cant determine Step Version' if stepver.nil?
-      step_version_item = json_step_item_from_yaml_hash(SafeYAML.load_file(path))
 
-      unless steps_and_versions[stepid]
-        steps_and_versions[stepid] = default_step_data_for_stepid(stepid)
-      end
-
+      # load
+      step_version_item = SafeYAML.load_file(path)
+      # add IDs
       step_version_item['steplib_source'] = options[:steplib_source]
       step_version_item['version_tag'] = stepver
       step_version_item['id'] = stepid
@@ -204,6 +128,13 @@ Find.find(options[:step_collection_folder]) do |path|
       if File.exist?(step_icon_file_path_256)
         step_version_item['icon_url_256'] = "#{options[:step_assets_url_root]}/#{stepid}/assets/icon_256.png"
       end
+      # validate and whitelist
+      step_version_item = json_step_item_from_yaml_hash(step_version_item)
+
+      unless steps_and_versions[stepid]
+        steps_and_versions[stepid] = default_step_data_for_stepid(stepid)
+      end
+
       steps_and_versions[stepid]['versions'] << step_version_item
     end
   end
